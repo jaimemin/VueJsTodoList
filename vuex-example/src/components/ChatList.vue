4301e454b45df45378ac06433e84df441c5f5f38
jaimemin <jaimemin@gmail.com>
2021-01-03 15:56:04 +0900

    vuex-example sample code

---
+++ vuex-example/src/components/ChatList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="list-group">
+            <template v-for="chat in chatList">
+                <ChatListItem
+                    :chat="chat"
+                    @click="itemClick"
+                    v-bind:key="chat.id"
+                />
+            </template>
+        </div>
+    </div>
+</template>
+<script>
+import ChatListItem from './ChatListItem';
+
+export default {
+    props: ['chatList'],
+    methods: {
+        itemClick(chatItem) {
+            this.$emit('read-item', chatItem);
+        }
+    },
+    components: {
+        ChatListItem
+    }
+}
+</script>
